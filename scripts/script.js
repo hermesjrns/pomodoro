94254cdc2cb332ad85034664ec9ea91d25442101
hermesjrns <hermesjrns@gmail.com>
2025-06-01 18:31:20 -0300

    features/new time modes, trigger audio at end & pg

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,37 @@
-class Main {
-    constructor(a, b){
-        this.pomodoro = new Pomodoro(a, b)
+function getLastInstance(study, rest, reset){
+    const instance = Pomodoro.instances[Pomodoro.instances.length-1]
+    if(reset) return instance
+    if(instance == undefined){
+        Pomodoro.instances = []
+        return new Pomodoro(study, rest).startStudy()
+    }else{
+        instance.changeTimers(study, rest)
+        return instance.startStudy()
     }
-    startStudy(){
-        this.pomodoro.startStudy()
-        this.pomodoro.changePhase("Study")
-    }
-    startRest(){
-        this.pomodoro.startRest()
-        this.pomodoro.changePhase("Rest")
-    }
-    next(){
-        this.pomodoro.nextPhase()
-    }
-}
 
+}
 class Pomodoro {
+    static instances = []
     constructor(study_time = 15, rest_time = 5) {
         this.study = study_time
         this.rest = rest_time
-        this.phase = 0
+        Pomodoro.instances = [this]
+        this.interval = 0
+        this.ms = parseInt(1000)
+        this.ringing = false
     }
+
+    actual_phase = 'Study'
+
+
+    
     ringBell(){
         document.querySelector('#bell').play()
     }
-    // nextPhase(){
-    //     this.phase ++
-    //     this.changePhase(phases[this.phase])
-    //     this.startRest()
-    // }
     
-    // static phases = ["Study", "Rest"]
+    stopBell(){
+        document.querySelector('#bell').pause()
+    }
 
     changeTime(text) {
         document.querySelector('div#time').innerHTML = text
@@ -42,38 +43,60 @@ class Pomodoro {
 
     insertButton(phase){
         if(phase === 'Study'){
-            return `<button id="next" class="bg-green-900 rounded p-2 active:bg-blue-300 hover:cursor-pointer font-xl text-red-600 mt-1" type="button" onclick="new Main().startRest()"> Start Rest </button`
+            // return `<button id="next" class="bg-green-900 rounded p-2 active:bg-blue-300 hover:cursor-pointer font-xl text-red-600 mt-1" type="button" onclick="new Main().startRest()"> Start Rest </button`
+            return `<button id="next" class="bg-green-900 rounded p-2 active:bg-blue-300 hover:cursor-pointer font-xl text-red-600 mt-1" type="button" onclick="getLastInstance(0,0,true).startRest()"> Start Rest </button`
 
         }
         if(phase === 'Rest'){
-            return `<button id="next" class="bg-green-900 rounded p-2 active:bg-blue-300 hover:cursor-pointer font-xl text-red-600 mt-1" type="button" onclick="new Main().startStudy()"> Start Study </button`
+             return `<button id="next" class="bg-green-900 rounded p-2 active:bg-blue-300 hover:cursor-pointer font-xl text-red-600 mt-1" type="button" onclick="getLastInstance(0,0,true).startStudy()"> Restart </button`
         }
         return ``
     }
-
+    changeTimers(stud, res){
+        this.study = parseInt(stud)
+        this.rest = parseInt(res)
+    }
 
     startRest(){
+        this.actual_phase = 'Rest'
+        this.changePhase(this.actual_phase)
+        this.stopBell()
         this.timer(this.rest)
+
     }
 
     startStudy(){
-        this.study
+        this.actual_phase = 'Study'
+        this.changePhase(this.actual_phase)
+        clearInterval(this.interval)
+        this.stopBell()
         this.timer(this.study)
     }
 
     stop = (id) => clearInterval(id)
     
+    timer_list = []
+
     timer(t) {
         let min = parseInt(t) - 1
-        let sec = 59
+        let sec = parseInt(59)
+        this.changeTime(`${min}:${(sec).toString().padStart(2, '0')}`)
+        let debug_html = document.getElementById('debugger')
+        let debugg = parseInt(debug_html.value)
+        let ms = this.ms
+        if(debugg){
+            ms = debugg
+        }
+       
         let timer_id = setInterval(() => {
             let string = `${min}:${sec.toString().padStart(2, '0')}`
             this.changeTime(string)
             sec--
             if (min.valueOf() < 0) {
-                const phase_txt = document.querySelector('div#phase').innerHTML
+                // const phase_txt = document.querySelector('div#phase').innerHTML
                 this.ringBell()
-                this.changePhase(this.insertButton(phase_txt))
+                this.changePhase(this.insertButton(this.actual_phase))
+                // console.log(phase_txt)
                 this.changeTime("Click above")
                 this.stop(timer_id)
             }
@@ -81,6 +104,20 @@ class Pomodoro {
                 min--
                 sec = 59
             }
-        }, 1000);
+            // console.log(`ms: ${ms}`)
+            // change this to change speed in miliseconds
+        }, ms);
+        this.interval = timer_id
+    }
+}
+
+function showDebugger(){
+    const checkbox = document.querySelector(`#debug_show`).valueOf().checked
+    let box = document.querySelector('div#debug_wrapper')
+    if(checkbox){
+        box.removeAttribute("hidden")
+        return
+    }else{
+        box.setAttribute("hidden",'true')
     }
 }
